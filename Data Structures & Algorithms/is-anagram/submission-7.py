ce49732393ba06e5fa129a7fe47aaccaaa3c0bae
Pranav Konjeti <107774675+PkTheCoda@users.noreply.github.com>
2026-05-10 17:03:03 -0500

    Add: is-anagram - submission-7

---
+++ Data Structures & Algorithms/is-anagram/submission-7.py
@@ -0,0 +1,22 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+
+        s_dict = {}
+        t_dict = {}
+
+        for char in s:
+            print(char)
+            if char in s_dict:
+                s_dict[char] += 1
+            else:
+                s_dict[char] = 1
+        
+        for char in t:
+            print(char)
+            if char in t_dict:
+                t_dict[char] += 1
+            else:
+                t_dict[char] = 1
+        
+        return s_dict == t_dict
+        
