e7c2c42b5f547ae2c90d01a6c98c74f7e5063e86
Pranav Konjeti <107774675+PkTheCoda@users.noreply.github.com>
2026-05-18 23:03:13 -0500

    Add: string-encode-and-decode - submission-3

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-3.py
@@ -0,0 +1,35 @@
+class Solution:
+
+    def encode(self, strs: List[str]) -> str:
+        # jump2 play me 
+        # 5jump24play2me
+
+        res = ""
+        for string in strs:
+            created = str(len(string)) + "#" + string
+
+            res += created
+        
+
+        return res
+
+    def decode(self, s: str) -> List[str]:
+
+        res = []
+
+        while len(s) != 0:
+
+            len_num = ""
+            index = 0
+            while s[index] != "#":
+                len_num += s[index]
+                index += 1
+
+
+            first_num = int(len_num)
+            first_word = s[index + 1:first_num + index + 1]
+            res.append(first_word)
+
+            s = s[(index + 1 + first_num):]
+
+        return res
