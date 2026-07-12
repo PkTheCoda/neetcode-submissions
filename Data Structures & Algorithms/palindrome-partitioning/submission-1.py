2dd930948554b96c20e1b1950b7e169f18cd5c46
Pranav Konjeti <107774675+PkTheCoda@users.noreply.github.com>
2026-07-11 19:19:18 -0500

    Add: palindrome-partitioning - submission-1

---
+++ Data Structures & Algorithms/palindrome-partitioning/submission-1.py
@@ -0,0 +1,26 @@
+class Solution:
+    def partition(self, s: str) -> List[List[str]]:
+        # aab
+
+        # dfs(path, index, combined_len)
+        # dfs(["a"], 1, 1)
+
+        res = []
+
+        def dfs(path, index):
+            if index == len(s):
+                res.append(list(path))
+                return
+            
+            for i in range(index, len(s)):
+                to_append = s[index:i+1]
+                is_palindrome = (to_append == to_append[::-1])
+                
+                if is_palindrome:
+                    path.append(to_append)
+                    dfs(path, i + 1)
+                    path.pop()
+            
+        
+        dfs([], 0)
+        return res
