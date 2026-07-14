5eebf9cc692f6cb247f59af7b7e5f7823fec0a67
Pranav Konjeti <107774675+PkTheCoda@users.noreply.github.com>
2026-07-13 22:09:56 -0500

    Add: count-number-of-islands - submission-3

---
+++ Data Structures & Algorithms/count-number-of-islands/submission-3.py
@@ -0,0 +1,28 @@
+from collections import deque
+class Solution:
+    def numIslands(self, grid: List[List[str]]) -> int:
+        visited = set()
+        count = 0
+        directions = [(0, 1), (0, -1), (1, 0), (-1, 0)]
+
+        def dfs(row, col):
+            
+            visited.add((row, col))
+
+            for cr, cc in directions:
+                nr, nc = row + cr, col + cc
+
+                if nr >= 0 and nc >= 0 and nr < len(grid) and nc < len(grid[nr]) and (nr, nc) not in visited and grid[nr][nc] == "1":
+                    dfs(nr, nc)
+            
+
+
+        for row in range(len(grid)):
+            for col in range(len(grid[row])):
+                if (row, col) not in visited and grid[row][col] == "1":
+                    count += 1
+
+                    dfs(row, col)
+        
+        return count
+        
